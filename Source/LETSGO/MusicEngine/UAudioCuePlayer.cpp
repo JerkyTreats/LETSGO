88fdb214c54b22eb8d9293b05e9462e08d2d3be5
jerkytreats.dev <public@jerkytreats.dev>
2024-05-30 10:57:27 -0700

    Throw more sauce at the wall. Nothing working nor tested, but doth compiles.

---
+++ Source/LETSGO/MusicEngine/UAudioCuePlayer.cpp
@@ -0,0 +1,83 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UAudioCuePlayer.h"
+
+#include "AAudioPlatform.h"
+#include "LETSGO/GameModes/ALetsGoGameMode.h"
+
+
+class ALetsGoGameMode;
+// Sets default values for this component's properties
+UAudioCuePlayer::UAudioCuePlayer()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UAudioCuePlayer::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Initialize Metronome
+	const ALetsGoGameMode* GameMode = Cast<ALetsGoGameMode>(GetWorld()->GetAuthGameMode());
+	Clock = GameMode->GetMainClock();
+
+	// Bind to FAudioPlatformTriggerDelegate
+	AudioPlatformReference->OnAudioPlatformTriggered.AddDynamic(this, &UAudioCuePlayer::OnAudioPlatformTriggered);
+
+	/** Defines settings for the FQuartzQuantizationBoundary structure. */
+	FQuartzQuantizationBoundary QuartzQuantizationBoundary;
+	QuartzQuantizationBoundary.Quantization = EQuartzCommandQuantization::Bar;
+	QuartzQuantizationBoundary.Multiplier = 1.0f;
+	QuartzQuantizationBoundary.CountingReferencePoint = EQuarztQuantizationReference::BarRelative;
+	QuartzQuantizationBoundary.bResetClockOnQueued = true;
+
+	/*
+	 *  1. Get Main Clock
+	 *  2. Subscribe to Quantization Event
+	 *		a. Clock
+	 *		b. In Quantization Boundary
+	 *		c. On Quantization Event Delegate
+	 *  3. On Audio Platform Triggered
+	 *		a. Map Note to Audio Cue
+	 *		b. Play Quantized Audio Cue
+	 *
+	 * 
+	 */
+}
+
+void UAudioCuePlayer::OnAudioPlatformTriggered(FLetsGoMusicNotes Note)
+{
+	PlayNote(Note);
+}
+
+void UAudioCuePlayer::PlayNote(FLetsGoMusicNotes Note)
+{
+
+	// Clock->SubscribeToQuantizationEvent(GetWorld(),EQuartzCommandQuantization::Bar,)
+}
+
+void UAudioCuePlayer::OnBar(FName ClockName, EQuartzCommandQuantization QuantizationType, int32 NumBars, int32 Beat,
+	float BeatFraction)
+{
+}
+
+void UAudioCuePlayer::FPlayQuantizedDelegate(EQuartzCommandDelegateSubType EventType, FName Name)
+{
+}
+
+// Called every frame
+void UAudioCuePlayer::TickComponent(float DeltaTime, ELevelTick TickType,
+                                       FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
---
+++ Source/LETSGO/MusicEngine/UAudioCuePlayer.h
@@ -0,0 +1,56 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ULetsGoMusicEngine.h"
+#include "Components/ActorComponent.h"
+#include "Quartz/AudioMixerClockHandle.h"
+#include "UAudioCuePlayer.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class LETSGO_API UAudioCuePlayer : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UAudioCuePlayer();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditInstanceOnly,BlueprintReadWrite)
+	class AAudioPlatform* AudioPlatformReference;
+
+	UFUNCTION()
+	void OnAudioPlatformTriggered(FLetsGoMusicNotes Note);
+
+	UFUNCTION()
+	void PlayNote(FLetsGoMusicNotes Note);
+
+	UFUNCTION()
+	void OnBar(FName ClockName, EQuartzCommandQuantization QuantizationType, int32 NumBars, int32 Beat, float BeatFraction);
+
+	// https://abovenoisestudios.com/blogeng/metasquartzverticalengp2
+	/**
+	 * @brief Function delegate. Triggers its functionality synced with the clock.
+	 * @param EventType Use a switch on this enumeration to select "CommandOnQueued". 
+	 * @param Name 
+	 */
+	UFUNCTION()
+	void FPlayQuantizedDelegate(EQuartzCommandDelegateSubType EventType, FName Name);
+
+	/** Used to bind the FPlayQuantizedDelegate in the class constructor. */
+	FOnQuartzCommandEventBP PlayQuantizationDelegate;
+	
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+
+	UPROPERTY(VisibleAnywhere, Category = "LETSGO | State")  
+	UQuartzClockHandle* Clock;
+};
