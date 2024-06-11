756a07d034761c2a02e15ff33be1bc70e7248364
jerkytreats.dev <public@jerkytreats.dev>
2024-06-11 06:39:26 -0700

    WIP: Start looking at AudioPlatformSpawner refactor requirements

--- Source/LETSGO/Phases/SetTonic.cpp
+++ Source/LETSGO/Phases/SetTonic.cpp
@@ -3,12 +3,29 @@
 
 #include "SetTonic.h"
 
+#include "Logging/StructuredLog.h"
+
+void USetTonic::OnAudioPlatformTriggered(FLetsGoMusicNotes IncomingNote)
+{
+	
+}
+
+void USetTonic::SetTonic(FLetsGoMusicNotes Note)
+{
+	
+}
+
 void USetTonic::Initialize()
 {
 }
 
 void USetTonic::Activate()
 {
+	// Spawn three Audio Platforms 
+	for (int i = 0; i <=3; i++)
+	{
+		AAudioPlatform* next = GetWorld()->SpawnActor<AAudioPlatform>(AAudioPlatform::StaticClass(), )
+	}
 }
 
 void USetTonic::Deactivate()
