47e3053dce1e478e9ba04bf20cb3b713069172df
jerkytreats.dev <public@jerkytreats.dev>
2024-07-28 07:28:39 -0700

    Start rebuilding drums in code

---
+++ Source/LETSGO/Instruments/Drums/Drums.cpp
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Drums.h"
+
+#include "Components/AudioComponent.h"
+#include "LETSGO/GameModes/ALetsGoGameMode.h"
+
+
+// Sets default values
+ADrums::ADrums(): Clock(nullptr)
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	InstrumentAudioComponent = CreateDefaultSubobject<UAudioComponent>(TEXT("Audio Component"));
+	InstrumentAudioComponent->SetAutoActivate(false);
+	InstrumentAudioComponent->SetSound(InstrumentMetaSoundSource);
+}
+
+// Called when the game starts or when spawned
+void ADrums::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UE_LOG(LogTemp, Display, TEXT("Setting Drum MainClock from GameMode clock object"));
+	// Get Main Clock
+	const ALetsGoGameMode* GameMode = Cast<ALetsGoGameMode>(GetWorld()->GetAuthGameMode());
+	Clock = GameMode->GetMainClock();
+}
+
+// Called every frame
+void ADrums::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/LETSGO/Instruments/Drums/Drums.h
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Quartz/AudioMixerClockHandle.h"
+#include "MetasoundSource.h"
+#include "Drums.generated.h"
+
+// See https://abovenoisestudios.com/blogeng/metasquartzverticalengp2
+UCLASS()
+class LETSGO_API ADrums : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ADrums();
+
+	UPROPERTY(BlueprintReadWrite, Category="LETSGO")
+	UQuartzClockHandle* Clock;
+
+	UPROPERTY(BlueprintReadWrite, Category="LETSGO")
+	UAudioComponent* InstrumentAudioComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="LETSGO")
+	UMetaSoundSource* InstrumentMetaSoundSource;
+	
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
