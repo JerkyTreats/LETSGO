42bf506972f8d4e3584ba1d44a51b8fb46864a34
jerkytreats.dev <public@jerkytreats.dev>
2024-09-21 12:14:04 -0700

    Remove bad InstrumentData file

--- Source/LETSGO/Instruments/InstrumentData.cpp
+++
@@ -1,7 +0,0 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "InstrumentData.h"
-
-
-// Add default functionality here for any IInstrumentData functions that are not pure virtual.
