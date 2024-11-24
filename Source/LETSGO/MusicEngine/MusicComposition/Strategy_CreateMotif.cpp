ee97097867b0fc582754a75f09a7a55f6c52a1dc
jerkytreats.dev <public@jerkytreats.dev>
2024-11-24 13:05:14 -0800

    Extract CreateMotif data properties into struct
    The debugger was acting weird on a couple properties being set, the cause of which was a switch statement missing breaks.
    
    Anyways, I discovered that once I moved everything into a struct, which is something I wanted to do anyways.
    
    Testing has proceeded to CreateMotif debugging, which is proceeding decently.

--- Source/LETSGO/MusicEngine/MusicComposition/Strategy_CreateMotif.cpp
+++ Source/LETSGO/MusicEngine/MusicComposition/Strategy_CreateMotif.cpp
@@ -6,6 +6,106 @@
 #include "MusicComposerState.h"
 
 USTRUCT()
+struct FCreateMotifData
+{
+	UPROPERTY()
+	float TensionBudget = 0.3f;
+
+	UPROPERTY()
+	float OptimalTensionPoint = 0.5f;
+
+	UPROPERTY()
+	int OptimalTensionBonus = 25;
+
+	UPROPERTY()
+	int WithinTensionBudgetBonus = 50;
+
+	UPROPERTY()
+	FLetsGoGeneratedScale Scale;
+
+	UPROPERTY()
+	TMap<int, float>ScaleDegreeResolution;
+
+	UPROPERTY()
+	EQuartzCommandQuantization Division;
+
+	UPROPERTY()
+	int Beats;
+
+	UPROPERTY()
+	int MaxBeatStrength;
+
+	UPROPERTY()
+	TMap<int, float> BeatStrength;
+
+	FCreateMotifData()
+	{
+		// Culturally western music listeners are conditioned to desire certain notes to "resolve" to tonic
+		// The stronger the pull to resolve, the more tension created if we don't
+		// This map tries to represent that desire to resolve 
+		ScaleDegreeResolution.Add(0, 1.0f); // 1
+		ScaleDegreeResolution.Add(1, 0.5f); // 2
+		ScaleDegreeResolution.Add(2, 0.5f); // 2
+		ScaleDegreeResolution.Add(3, 0.4f); // 3
+		ScaleDegreeResolution.Add(4, 0.4f); // 3
+		ScaleDegreeResolution.Add(5, 0.6f); // 4
+		ScaleDegreeResolution.Add(6, 0.6f); // 4
+		ScaleDegreeResolution.Add(7, 0.8f); // 5
+		ScaleDegreeResolution.Add(8, 0.8f); // 5
+		ScaleDegreeResolution.Add(9, 0.3f); // 6
+		ScaleDegreeResolution.Add(10, 0.3f); // 6
+		ScaleDegreeResolution.Add(11, 0.9f); // 7
+		
+		const TArray SubDivisions {
+        	EQuartzCommandQuantization::QuarterNote,
+        	EQuartzCommandQuantization::EighthNote,
+        	EQuartzCommandQuantization::SixteenthNote,
+        };
+        
+        // This is choosing a subdivision at random
+        Division = SubDivisions[FMath::RandRange(0, SubDivisions.Num() - 1)];
+
+		switch (Division)
+        {
+        case EQuartzCommandQuantization::QuarterNote:
+        	Beats = 4;
+        	MaxBeatStrength = 0.25f;
+			break;
+        case EQuartzCommandQuantization::EighthNote:
+        	Beats = 8;
+        	MaxBeatStrength = 0.5f;
+			break;
+        case EQuartzCommandQuantization::SixteenthNote:
+        	Beats = 16;
+        	MaxBeatStrength = 0.75f;
+			break;
+        default:
+        	Beats = 0;
+			MaxBeatStrength = 0;
+        }
+
+		BeatStrength = TMap<int, float>();
+		
+		// For zero based arrays, an even number will resolve to the stronger beat of a larger scale degree
+		// 16th note [8] resolves to the 8th note [4] resolves to the quarter note [2]
+		// 16th note [6] resolves to the 8th note [3] which does not resolve to the quarter note
+		// So we recursively modulus check if note is even, increasing strength value each time its divisible
+		// This fills our BeatStrengths
+		for (int Beat = 0; Beat < Beats; Beat++)
+		{
+			const float Base = 0.25f;
+			float Strength = Base;
+			while(Beat % 2 != 0)
+			{
+				Strength += Base;
+				Beat /= 2;
+			}
+			BeatStrength.Add(Beat, Strength); // [ 0.75, 0.25, 0.5, 0.25, 0.75, 0.25, 0.5, 0.25 ] 16 beat strength array
+		}
+	}
+};
+
+USTRUCT()
 struct FNoteCandidate
 {
 	int Min;
@@ -23,76 +123,11 @@ struct FNoteCandidate
 };
 
 FInstrumentSchedule UStrategy_CreateMotif::GenerateInstrumentSchedule(FComposerData& CurrentComposerData,
-                                                   const AMusicComposerState* State, const int StartAtBar)
+                                                                      const AMusicComposerState* State, const int StartAtBar)
 {
-	FLetsGoGeneratedScale Scale = State->Scale;
-
-	// Culturally western music listeners are conditioned to desire certain notes to "resolve" to tonic
-	// The stronger the pull to resolve, the more tension created if we don't
-	// This map tries to represent that desire to resolve 
-	TMap<int, float>ScaleDegreeResolution;
-	ScaleDegreeResolution.Add(0, 1.0f); // 1
-	ScaleDegreeResolution.Add(1, 0.5f); // 2
-	ScaleDegreeResolution.Add(2, 0.5f); // 2
-	ScaleDegreeResolution.Add(3, 0.4f); // 3
-	ScaleDegreeResolution.Add(4, 0.4f); // 3
-	ScaleDegreeResolution.Add(5, 0.6f); // 4
-	ScaleDegreeResolution.Add(6, 0.6f); // 4
-	ScaleDegreeResolution.Add(7, 0.8f); // 5
-	ScaleDegreeResolution.Add(8, 0.8f); // 5
-	ScaleDegreeResolution.Add(9, 0.3f); // 6
-	ScaleDegreeResolution.Add(10, 0.3f); // 6
-	ScaleDegreeResolution.Add(11, 0.9f); // 7
-
-	const TArray SubDivisions {
-		EQuartzCommandQuantization::QuarterNote,
-		EQuartzCommandQuantization::EighthNote,
-		EQuartzCommandQuantization::SixteenthNote,
-	};
-
-	float MaxBeatStrength = 0;
-	// This is choosing a subdivision at random
-	EQuartzCommandQuantization Division = SubDivisions[FMath::RandRange(0, SubDivisions.Num() - 1)];
-	int Beats = 0;
+	const FCreateMotifData Data = FCreateMotifData();
 	
-	switch (Division)
-	{
-	case EQuartzCommandQuantization::QuarterNote:
-		Beats = 4;
-		MaxBeatStrength = 0.25f;
-	case EQuartzCommandQuantization::EighthNote:
-		Beats = 8;
-		MaxBeatStrength = 0.5f;
-	case EQuartzCommandQuantization::SixteenthNote:
-		Beats = 16;
-		MaxBeatStrength = 0.75f;
-	default:
-		Beats = 0;
-	}
-
-	// Just like music listeners are conditioned to desire certain notes to resolve
-	// We are also carefully attuned to the beat
-	// "1 e and a 2 e and a 3 e and a 4 e and a" represents a 16th note bar
-	// "1" is stronger than "and" is stronger than "e" or "a"
-	// This map attempts to represent that relative strength of beats among various subdivisions
-	TMap<int, float> BeatStrength;
-
-	// For zero based arrays, an even number will resolve to the stronger beat of a larger scale degree
-	// 16th note [8] resolves to the 8th note [4] resolves to the quarter note [2]
-	// 16th note [6] resolves to the 8th note [3] which does not resolve to the quarter note
-	// So we recursively modulus check if note is even, increasing strength value each time its divisible
-	// This fills our BeatStrengths
-	for (int Beat = 0; Beat < Beats; Beat++)
-	{
-		const float Base = 0.25f;
-		float Strength = Base;
-		while(Beat % 2 != 0)
-		{
-			Strength += Base;
-			Beat /= 2;
-		}
-		BeatStrength.Add(Beat, Strength); // [ 0.75, 0.25, 0.5, 0.25, 0.75, 0.25, 0.5, 0.25 ] 16 beat strength array
-	}
+	FLetsGoGeneratedScale Scale = State->Scale;
 
 	int Octave = FMath::RandRange(CurrentComposerData.OctaveMin, CurrentComposerData.OctaveMax);
 
@@ -102,17 +137,15 @@ FInstrumentSchedule UStrategy_CreateMotif::GenerateInstrumentSchedule(FComposerD
 	FNotesPerBar Test = FNotesPerBar(0, Note.SoundData);
 	Schedule.NotesInBar.Add(Test);
 
-	float MaxResolution = MaxBeatStrength + ScaleDegreeResolution.FindRef(0);
+	float MaxResolution = Data.MaxBeatStrength + Data.ScaleDegreeResolution.FindRef(0);
 	float CurrentResolution = MaxResolution;
-	float TensionBudget = 0.3f;
-	float OptimalTensionPoint = 0.5f;
-	int OptimalTensionBonus = 25;
-	int WithinTensionBudgetBonus = 50;
+
+	float CurrentTensionBudget = Data.TensionBudget;
 	
-	for (int i = 1; i < Beats; i++)
+	for (int i = 1; i < Data.Beats; i++)
 	{
 		// [0] = 0; [1] = 0.25, etc.
-		float Tension = MaxBeatStrength - BeatStrength[i];
+		float Tension = Data.MaxBeatStrength - Data.BeatStrength[i];
 		
 		// The higher the BeatTension, the more likely we are to rest
 		float DefaultRestChance = 0.25; // Convert to non-magic number (put in state)
@@ -128,7 +161,7 @@ FInstrumentSchedule UStrategy_CreateMotif::GenerateInstrumentSchedule(FComposerD
 		for ( int n = 0; n < State->AllowableNoteIndices.Num(); n++)
 		{
 			// ex. 2nd - 0.5
-			float ResolutionWeight = ScaleDegreeResolution.FindRef(State->AllowableNoteIndices[i]);
+			float ResolutionWeight = Data.ScaleDegreeResolution.FindRef(State->AllowableNoteIndices[i]);
 
 			float CandidateTotalResolution = CurrentResolution + ResolutionWeight + Tension; // 1.8
 			float CurrentMaxResolution = MaxResolution + CurrentResolution; // 2.0
@@ -140,17 +173,17 @@ FInstrumentSchedule UStrategy_CreateMotif::GenerateInstrumentSchedule(FComposerD
 			// If CT < TB, good
 			// If current beat count is low, leaving more budget is good
 			// If good, multiply chance to pick increased more than less good/bad.
-			float BeatPercentage = i / Beats; // Beats[5] = 0.31 - 31%
-			float DiffFromOptimalTensionPoint = FMath::Abs(OptimalTensionPoint - BeatPercentage); // .29
+			float BeatPercentage = i / Data.Beats; // Beats[5] = 0.31 - 31%
+			float DiffFromOptimalTensionPoint = FMath::Abs(Data.OptimalTensionPoint - BeatPercentage); // .29
 
-			int CandidateWeight = (CandidateTension * 100) + (OptimalTensionBonus * DiffFromOptimalTensionPoint); // 10 + 7
+			int CandidateWeight = (CandidateTension * 100) + (Data.OptimalTensionBonus * DiffFromOptimalTensionPoint); // 10 + 7
 
-			if (TensionBudget - CandidateTension > 0)
+			if (CurrentTensionBudget - CandidateTension > 0)
 			{
-				CandidateWeight += WithinTensionBudgetBonus;
+				CandidateWeight += Data.WithinTensionBudgetBonus;
 			}
 
-			Ranges.Add(FNoteCandidate(PickSum +1, PickSum + CandidateWeight, ResolutionWeight + BeatStrength[i], CandidateTension));
+			Ranges.Add(FNoteCandidate(PickSum +1, PickSum + CandidateWeight, ResolutionWeight + Data.BeatStrength[i], CandidateTension));
 			PickSum += CandidateWeight;
 		}
 
@@ -164,7 +197,7 @@ FInstrumentSchedule UStrategy_CreateMotif::GenerateInstrumentSchedule(FComposerD
 			{
 				SelectedNote = CurrentComposerData.InstrumentData.GetNote(Octave, State->Scale.Notes[State->AllowableNoteIndices[i]]);
 				CurrentResolution += Ranges[n].Resolution;
-				TensionBudget -= Ranges[n].Tension;
+				CurrentTensionBudget -= Ranges[n].Tension;
 				break;
 			}
 		}
@@ -175,7 +208,7 @@ FInstrumentSchedule UStrategy_CreateMotif::GenerateInstrumentSchedule(FComposerD
 	CurrentComposerData.CreateMotifCount++;
 	
 	TArray<FPerBarSchedule> PerBarSchedules = TArray { FPerBarSchedule(Schedule) };
-	FInstrumentSchedule InstrumentSchedule = FInstrumentSchedule(Division, PerBarSchedules, StartAtBar);
+	FInstrumentSchedule InstrumentSchedule = FInstrumentSchedule(Data.Division, PerBarSchedules, StartAtBar);
 	return InstrumentSchedule;
 }
 
@@ -187,7 +220,7 @@ float UStrategy_CreateMotif::GetStrategyAppropriateness(FComposerData& CurrentCo
 		return 0.0f;
 	}
 
-	float Weight = 0.5f;
+	float Weight = 0.8f;
 
 	if (CurrentComposerData.InstrumentRole == Bass)
 	{
