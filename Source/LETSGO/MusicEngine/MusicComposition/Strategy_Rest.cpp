3c66cdebd5d9fb323448af78b2e6868836367051
jerkytreats.dev <public@jerkytreats.dev>
2024-12-03 06:01:44 -0800

    Add Rest musical strategy

---
+++ Source/LETSGO/MusicEngine/MusicComposition/Strategy_Rest.cpp
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Strategy_Rest.h"
+
+#include "ComposerData.h"
+#include "MusicComposerState.h"
+#include "LETSGO/Instruments/InstrumentSchedule.h"
+
+FInstrumentSchedule UStrategy_Rest::GenerateInstrumentSchedule(FComposerData& CurrentComposerData, const AMusicComposerState* State, const int StartAtBar)
+{
+	
+	return FInstrumentSchedule();
+}
+
+float UStrategy_Rest::GetStrategyAppropriateness(FComposerData& CurrentComposerData, const AMusicComposerState* State)
+{
+	if (! CurrentComposerData.IsMultiNoteInstrument() || State->AllowableNoteIndices.Num() == 0)
+	{
+		return 1.0f;
+	}
+
+	float Weight = 0.3f;
+	
+	return Weight;
+}
+
+// This strategy doesn't need input from other instruments
+float UStrategy_Rest::GetInstrumentAppropriateness(FComposerData& CurrentComposerData,
+	const AMusicComposerState* State)
+{
+	return 0.0f;
+}
+
---
+++ Source/LETSGO/MusicEngine/MusicComposition/Strategy_Rest.h
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MusicComposerState.h"
+#include "MusicStrategy.h"
+#include "UObject/Object.h"
+#include "Strategy_Rest.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LETSGO_API UStrategy_Rest : public UObject, public IMusicStrategy
+{
+	GENERATED_BODY()
+
+public:
+	virtual FInstrumentSchedule GenerateInstrumentSchedule(FComposerData& CurrentComposerData, const AMusicComposerState* State, const int StartAtBar) override;
+	virtual float GetStrategyAppropriateness(FComposerData& CurrentComposerData, const AMusicComposerState* State) override;
+	virtual float
+	GetInstrumentAppropriateness(FComposerData& CurrentComposerData, const AMusicComposerState* State) override;
+};
